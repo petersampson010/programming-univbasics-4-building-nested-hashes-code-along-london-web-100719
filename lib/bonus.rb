c52bf7e04c11454c0ef59cea3e1a2212a121a24a
Peter Sampson <petersampson010@gmail.com>
2019-09-03 15:33:03 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,6 +19,8 @@ def bonus
       ]
    }
   }
-  epic_tragedy[:montague][:hero][:status] == "dead"
-  epic_tragedy[:capulet][:heroine][:status] == "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
+  return epic_tragedy
 end
